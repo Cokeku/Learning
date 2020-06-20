97ef67b5459bf998284a748773a03a0eea806877
Ingranzhao <1154283293@qq.com>
2020-06-20 14:00:45 +0800

    update file

---
+++ DataStructe/bsearch.py
@@ -0,0 +1,17 @@
+def bsearch(L,n,target):
+    low = 0
+    high = n - 1
+    while low <= high:
+        mid = int((low+high) / 2)
+        if L[mid] == target:
+            return mid 
+        elif (L[mid]) < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+if __name__ == "__main__":
+    arry = [4,20,21,50,60,71]
+    
+    print(bsearch(arry,len(arry),50))
